b610257450a60c0ff8d6c981fbb053c7bd086a47
Roberto Antelo <roberto_antelo@hotmail.com>
2022-12-10 04:59:25 -0600

    Create tests file for point of sales

---
+++ tests/PointOfSales.test.js
@@ -0,0 +1,14 @@
+import PointOfSales from '../src/PointOfSales';
+
+test('Point of Sales loads input correctly', () => {
+  const pointOfSales = new PointOfSales();
+
+  // pointOfSales.loadPurchase('./inputs/input1.csv');
+  // expect(pointOfSales.countSales()).toBe(1);
+
+  // pointOfSales.loadPurchase('./inputs/input2.csv');
+  // expect(pointOfSales.countSales()).toBe(2);
+
+});
+test('Point of Sales print Purchase correctly', () => { expect(true).toBe(true) });
+test('Point of Sales print History correctly', () => { expect(true).toBe(true) })
